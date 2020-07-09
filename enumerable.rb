55466a0d49c57dd9e48942087a5c5b578ed4903d
RNtaate <royhyde14@gmail.com>
2020-07-09 15:36:55 +0300

    add my_select

--- enumerable.rb
+++ enumerable.rb
@@ -2,12 +2,27 @@ module Enumerable
   # my_each method
   def my_each
     arr = to_a
-    arr.length.times { |i| yield arr[i] } if block_given? self
+    arr.length.times { |i| yield arr[i] } if block_given?
+    self
   end
 
+  # my_each_with_index
   def my_each_with_index
     array = to_a
-    array.length.times { |i| yield array[i], i } if block_given? self
+    array.length.times { |i| yield array[i], i } if block_given?
+    self
+  end
+
+  # my_select
+  def my_select
+    array = to_a
+    final_array = []
+    if block_given?
+      array.my_each { |i| final_array << i if yield i }
+      final_array = final_array.to_h if is_a? Hash
+      final_array
+    else
+      self
+    end
   end
 end
-
