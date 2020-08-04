b05df695455bea6e78f5404fd72ef3de1c355c0a
Mark Alexander <6917497+m15e@users.noreply.github.com>
2020-08-04 22:24:04 +0800

    Preliminary linter fixes

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -7,60 +7,60 @@ describe Enumerable do
   let(:word_array) { %w{ a string very_long_string } }
   
   # my_each
-  describe "#my_each" do
-    it "returns the same value as the original given an array and a block" do
+  describe '#my_each' do
+    it 'returns the same value as the original given an array and a block' do
       expect(spec_num_array.my_each { |n| n * 2 }).to eql(spec_num_array.each { |n| n * 2 })
     end
 
-    it "returns the same value as the original given a range and a block" do
+    it 'returns the same value as the original given a range and a block' do
       expect(spec_range.my_each { |n| n * 2 }).to eql(spec_range.each { |n| n * 2 })
     end
 
-    it "returns the same value as the original given a hash and a block" do
+    it 'returns the same value as the original given a hash and a block' do
       expect(spec_hash.my_each { |n| n * 2 }).to eql(spec_hash.each { |n| n * 2 })
     end    
   end
 
   # my_each_with_index
-  describe "#my_each_with_index" do
-    it "returns the same value as the original given an array and a block" do
+  describe '#my_each_with_index' do
+    it 'returns the same value as the original given an array and a block' do
       expect(spec_num_array.my_each_with_index { |a,b| a + b }).to eql(spec_num_array.each_with_index { |a,b| a + b })
     end
 
-    it "returns the same value as the original given a range and a block" do
+    it 'returns the same value as the original given a range and a block' do
       expect(spec_range.my_each_with_index { |a,b| a + b }).to eql(spec_range.each_with_index { |a,b| a + b })
     end
 
   end
 
   # my_select
-  describe "#my_select" do
-    it "returns the same value as the original given an array and a block" do
+  describe '#my_select' do
+    it 'returns the same value as the original given an array and a block' do
       expect(spec_num_array.my_select { |v| v.even? }).to eql(spec_num_array.select { |v| v.even? })
     end
 
-    it "returns the same value as the original given a range and a block" do
+    it 'returns the same value as the original given a range and a block' do
       expect(spec_range.my_select { |v| v.even? }).to eql(spec_range.select { |v| v.even? })
     end
 
-    it "returns the same value as the original given a hash and a block" do
+    it 'returns the same value as the original given a hash and a block' do
       expect(spec_hash.my_select { |k,v| v.even? }).to eql(spec_hash.select { |k,v| v.even? })
     end
   end
 
   # my_all
-  describe "my_all" do
-    it "returns true if all values are smaller than 21" do
+  describe 'my_all' do
+    it 'returns true if all values are smaller than 21' do
       expect(spec_num_array.my_all? { |v| v < 21 } ).to eql(spec_num_array.all? { |v| v < 21 })
     end
 
-    it "returns false if a value in the array doesn't satisfy the block condition" do
+    it 'returns false if a value in the array doesn't satisfy the block condition' do
       expect(spec_num_array.my_all? { |v| v < 10 } ).to eql(spec_num_array.all? { |v| v < 10 })
     end
   end
 
   # my_none
-  describe "#my_none?" do
+  describe '#my_none?' do
     it 'return false if any of the elements in the array is equal to true.' do
       expect(spec_num_array.my_none? { |el| el.even? }).to eql(false)
     end
@@ -75,7 +75,7 @@ describe Enumerable do
   end
 
   # my_any
-  describe "#my_any" do
+  describe '#my_any' do
     it 'returns true if one value in the array is true.' do
       expect(spec_num_array.my_any?(2)).to eql(true)
     end
