861d504b8c5542e7c1b18f716342f60edd7fae24
Mark Alexander <6917497+m15e@users.noreply.github.com>
2020-08-04 22:40:17 +0800

    FIX linter errors

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -3,32 +3,32 @@ require 'enumerable'
 describe Enumerable do
   let(:spec_num_array) { [*1..20] }
   let(:spec_range) { (1..20) }
-  let(:spec_hash) { { a: 1, b: 2, c: 3, d: 4 } }    
-  let(:word_array) { %w{ a string very_long_string } }
+  let(:spec_hash) { { a: 1, b: 2, c: 3, d: 4 } }
+  let(:word_array) { %w{a string very_long_string} }
   
   # my_each
   describe '#my_each' do
     it 'returns the same value as the original given an array and a block' do
-      expect(spec_num_array.my_each { |n| n * 2 }).to eql(spec_num_array.each { |n| n * 2 })
+      expect(spec_num_array.my_each { |n| n + 2 }).to eql(spec_num_array.each { |n| n + 2 })
     end
 
     it 'returns the same value as the original given a range and a block' do
-      expect(spec_range.my_each { |n| n * 2 }).to eql(spec_range.each { |n| n * 2 })
+      expect(spec_range.my_each { |n| n + 2 }).to eql(spec_range.each { |n| n + 2 })
     end
 
     it 'returns the same value as the original given a hash and a block' do
-      expect(spec_hash.my_each { |n| n * 2 }).to eql(spec_hash.each { |n| n * 2 })
-    end    
+      expect(spec_hash.my_each { |n| n + 2 }).to eql(spec_hash.each { |n| n + 2 })
+    end
   end
 
   # my_each_with_index
   describe '#my_each_with_index' do
     it 'returns the same value as the original given an array and a block' do
-      expect(spec_num_array.my_each_with_index { |a,b| a + b }).to eql(spec_num_array.each_with_index { |a,b| a + b })
+      expect(spec_num_array.my_each_with_index { |a, b| a + b }).to eql(spec_num_array.each_with_index { |a, b| a + b })
     end
 
     it 'returns the same value as the original given a range and a block' do
-      expect(spec_range.my_each_with_index { |a,b| a + b }).to eql(spec_range.each_with_index { |a,b| a + b })
+      expect(spec_range.my_each_with_index { |a, b| a + b }).to eql(spec_range.each_with_index { |a, b| a + b })
     end
 
   end
@@ -44,33 +44,33 @@ describe Enumerable do
     end
 
     it 'returns the same value as the original given a hash and a block' do
-      expect(spec_hash.my_select { |k,v| v.even? }).to eql(spec_hash.select { |k,v| v.even? })
+      expect(spec_hash.my_select { |_k, v| v.even? }).to eql(spec_hash.select { |_k, v| v.even? })
     end
   end
 
   # my_all
   describe 'my_all' do
     it 'returns true if all values are smaller than 21' do
-      expect(spec_num_array.my_all? { |v| v < 21 } ).to eql(spec_num_array.all? { |v| v < 21 })
+      expect(spec_num_array.my_all? { |v| v < 21 }).to eql(spec_num_array.all? { |v| v < 21 })
     end
 
     it 'returns false if a value in the array doesn\'t satisfy the block condition' do
-      expect(spec_num_array.my_all? { |v| v < 10 } ).to eql(spec_num_array.all? { |v| v < 10 })
+      expect(spec_num_array.my_all? { |v| v < 10 }).to eql(spec_num_array.all? { |v| v < 10 })
     end
   end
 
   # my_none
   describe '#my_none?' do
     it 'return false if any of the elements in the array is equal to true.' do
-      expect(spec_num_array.my_none? { |el| el.even? }).to eql(false)
+      expect(spec_num_array.my_none?(&:even?).to eql(false)
     end
 
     it 'return false if any of the elements in the range is equal to true.' do
-      expect(spec_range.my_none? { |el| el.even? }).to eql(false)
+      expect(spec_range.my_none?(&:even?).to eql(false)
     end
 
     it 'return false if any of the elements in the hash is equal to true.' do
-      expect(spec_hash.my_none? { |key, val| val > 2  }).to eql(false)
+      expect(spec_hash.my_none? { |_key, val| val > 2 }).to eql(false)
     end
   end
 
@@ -85,7 +85,7 @@ describe Enumerable do
     end
 
     it 'returns true if one key in the hash is true.' do
-      expect(spec_hash.my_any? { |key, val| key == :a }).to eql(true)
+      expect(spec_hash.my_any? { |key, _val| key == :a }).to eql(true)
     end
   end
 
@@ -96,11 +96,11 @@ describe Enumerable do
     end
 
     it 'counts the number of items in a range that match the given argument or block.' do
-      expect(spec_range.my_count { |el| el.even? }).to eql(10)
+      expect(spec_range.my_count(&:even?).to eql(10)
     end
 
     it 'counts the number of key value pairs in a hash that match the given argument or block.' do
-      expect(spec_hash.my_count { |key, val| key == :a }).to eql(1)
+      expect(spec_hash.my_count { |key, _val| key == :a }).to eql(1)
     end
   end
 
@@ -115,7 +115,7 @@ describe Enumerable do
     end
 
     it 'iterates over every element in an hash & performs an action on the keys or values ' do
-      expect(spec_hash.my_map { |el, val| val * 2 }).to eql(spec_hash.map { |el, val| val * 2 })
+      expect(spec_hash.my_map { |_el, val| val * 2 }).to eql(spec_hash.map { |_el, val| val * 2 })
     end
   end
 
@@ -130,8 +130,7 @@ describe Enumerable do
     end
 
     it 'finds the longest string' do
-      expect(word_array.my_inject { |hold, compare| hold.length > compare.length ? hold : compare }).to eql('very_long_string')
+      expect(word_array.my_inject { |a, b| a.length > b.length ? a : b }).to eql('very_long_string')
     end
   end
-
 end
