dcbbd6b5b5815cae1fed4f43f62d17caeedc2140
Mark Alexander <6917497+m15e@users.noreply.github.com>
2020-08-04 23:04:32 +0800

    Fix linter issues

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -5,19 +5,21 @@ describe Enumerable do
   let(:spec_range) { (1..20) }
   let(:spec_hash) { { a: 1, b: 2, c: 3, d: 4 } }
   let(:word_array) { %w[a string very_long_string] }
+  let(:test_array) { [*1..20].each { |v| v + 1 } }
+  let(:test_range) { (1..20).each { |v| v + 1 } }
   
   # my_each
   describe '#my_each' do
     it 'returns the same value as the original given an array and a block' do
-      expect(spec_num_array.my_each { |n| + 2 }).to eql(spec_num_array.each { |n| + 2 })
+      expect(spec_num_array.my_each { |v| v + 1 }).to eql(test_array)
     end
 
     it 'returns the same value as the original given a range and a block' do
-      expect(spec_range.my_each { |n| n + 2 }).to eql(spec_range.each { |n| n + 2 })
+      expect(spec_range.my_each { |v| v + 1 }).to eql(test_range)
     end
 
     it 'returns the same value as the original given a hash and a block' do
-      expect(spec_hash.my_each { |_k, v| v + 2 }).to eql(spec_hash.each { |_k, v| v + 2 })
+      expect(spec_hash.my_each { |_k, v| v + 1 }).to eql(spec_hash.each { |_k, v| v + 1 })
     end
   end
 
