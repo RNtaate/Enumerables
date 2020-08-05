654997fc6e4ce97b7f86f1c73d3cb148a2772336
jacobrees <jacob.rees97@yahoo.com>
2020-08-05 10:17:48 +0100

    Add rspec my_each_with_index method test cases

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -40,4 +40,34 @@ describe Enumerable do
       expect(range.my_each(&pr)).to eql(range)
     end
   end
+
+  describe "#my_each_with_index" do
+    it "returns an Enumerator when no block is given" do
+      expect(array.my_each_with_index).to be_an(Enumerator)
+    end
+
+    it "returns self when self is an array and  block is given" do
+      expect(array.my_each_with_index{|num| num}).to eql(array)
+    end
+
+    it "returns self when self is a hash and  block is given" do
+      expect(hash.my_each_with_index{|num| num}).to eql(hash)
+    end
+
+    it "returns self when self is a range and  block is given" do
+      expect(range.my_each_with_index{|num| num}).to eql(range)
+    end
+
+    it "returns self when self is an array and proc is given" do
+      expect(array.my_each_with_index(&pr)).to eql(array)
+    end
+
+    it "returns self when self is a hash and proc is given" do
+      expect(hash.my_each_with_index(&pr)).to eql(hash)
+    end
+
+    it "returns self when self is a hash and proc is given" do
+      expect(range.my_each_with_index(&pr)).to eql(range)
+    end
+  end
 end
